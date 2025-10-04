a4a0c80d12368c9e55544f34412ed6c6496ad019
Johann Bandelow <johannabandelow@gmail.com>
2025-10-04 11:32:04 -0300

    configura testes de integração, cria docker-compose, troca banco para postgres (preciso da feature de schema), adiciona migrations

---
+++ src/test/java/com/fiapchallenge/garage/integration/BaseIntegrationTest.java
@@ -0,0 +1,9 @@
+package com.fiapchallenge.garage.integration;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("test")
+public abstract class BaseIntegrationTest {
+}
